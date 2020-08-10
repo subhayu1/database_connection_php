2e960ff3d3d14e3d3661929f97f7456fc1f66421
subhayu1 <subhayu.rimal@gmail.com>
2020-08-09 19:09:07 -0700

    changed username and pw

--- connect.php
+++ connect.php
@@ -4,16 +4,16 @@
 
 
 	// Database connection
-	$conn = new mysqli('34.67.14.209','webform','password','webform');
+	$conn = new mysqli('webform@34.67.14.209','webform','password','webform');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt = $conn->prepare("insert into registration(firstName, lastName) values(?, ?)");
 		$stmt->bind_param("sssssi", $firstName, $lastName);
 		$execval = $stmt->execute();
 		echo $execval;
-		echo "Registration successfully...";
+		echo "Registration successful...";
 		$stmt->close();
 		$conn->close();
 	}
